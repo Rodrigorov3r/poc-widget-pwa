0dfc05ba86d130c7880cb05b26cdda1cf2967a67
roacevedo <racevedo@andreani.com>
2025-02-18 17:15:04 -0300

    fix: config iniciales pwa

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/mi_andreani_512.png',
+        '/mi_andreani_256.png',
+        '/mi_andreani_384.png',
+        '/mi_andreani_192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- src/index.tsx
+++ src/index.tsx
@@ -11,3 +11,16 @@ if (rootEl) {
     </React.StrictMode>,
   );
 }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registrado con éxito:', registration);
+      })
+      .catch((error) => {
+        console.error('Error al registrar el Service Worker:', error);
+      });
+  });
+}
